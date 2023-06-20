ca343081c5b640d9a5d2ef59e4de57dc4fc8e6cf
mharrish7 <harrishkumar100@gmail.com>
2023-06-20 10:47:42 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import 'axios';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [text, setText] = useState("");
+
+  const getTest = async () => {
+    let t = await axios.get("/api/test");
+    setText(t.data.data);
+  }
+
+  useEffect(() => {
+    getTest();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{text}</h1>
     </div>
   );
 }
